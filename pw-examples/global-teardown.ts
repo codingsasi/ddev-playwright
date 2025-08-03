018c1559f57e129cb6b582186807780bd7c91f82
Abhai Sasidharan <mail@abh.ai>
2025-08-03 11:31:07 -0400

    Surround try catch to supress missing drush errors

--- pw-examples/global-teardown.ts
+++ pw-examples/global-teardown.ts
@@ -9,15 +9,27 @@ async function globalTeardown(config: FullConfig) {
     // Clean up test users and data in CI environment.
     // Uses platform CLI to run drush commands in platform.sh. Use terminus in pantheon, etc,
     // or simple ssh like this: ssh -t user@domain.example 'cd /var/www/html && vendor/bin/drush status'
-    execSync(`platform -edev drush status`, { stdio: 'inherit' });
+    try {
+      execSync(`platform -edev drush status`, { stdio: 'inherit' });
+    } catch (error) {
+      console.log('Platform CLI or drush not available in CI');
+    }
   } else {
     if (isDdev !== false) {
       console.log('Running in ddev container');
-      execSync('drush status', { stdio: 'inherit' });
+      try {
+        execSync('drush status', { stdio: 'inherit' });
+      } catch (error) {
+        console.log('drush not available');
+      }
     }
     else {
       console.log('Running from host, outside ddev container.');
-      execSync('ddev drush status', { stdio: 'inherit' });
+      try {
+        execSync('ddev drush status', { stdio: 'inherit' });
+      } catch (error) {
+        console.log('ddev drush not available');
+      }
     }
   }
 }
